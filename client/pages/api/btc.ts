126c5b224a4a28c02d5417eca7eab6940ca1fe50
KumailB <sxb180119@utdallas.edu>
2021-11-23 02:15:03 -0600

    feat: btc price fetch by jonathan

--- client/pages/api/btc.ts
+++ client/pages/api/btc.ts
@@ -1,5 +1,9 @@
+let btc_price_cache: number = 56031.40;
 
 export const getBtcRate = async (): Promise<number> => {
-  const rate = 59634.0989
-  return rate;
+  const rate = fetch('https://api.coindesk.com/v1/bpi/currentprice.json').
+  then(data => data.json()).then(data => data['bpi']['USD']['rate'])
+      .then(data => Number(data.replace(/[^0-9.-]+/g,"")))
+      .then(data => {btc_price_cache = data as number;  return data as number});
+  return btc_price_cache;
 }
