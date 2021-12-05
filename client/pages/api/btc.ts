b0c990765fd87a1ad8128af6236f32b0725de506
KumailB <sxb180119@utdallas.edu>
2021-12-04 18:32:08 -0600

    refactor: heorku build refactors

--- client/pages/api/btc.ts
+++
@@ -1,9 +0,0 @@
-let btc_price_cache: number = 56031.40;
-
-export const getBtcRate = async (): Promise<number> => {
-  const rate = fetch('https://api.coindesk.com/v1/bpi/currentprice.json').
-  then(data => data.json()).then(data => data['bpi']['USD']['rate'])
-      .then(data => Number(data.replace(/[^0-9.-]+/g,"")))
-      .then(data => {btc_price_cache = data as number;  return data as number});
-  return btc_price_cache;
-}
--- client/pages/api/graphql/index.ts
+++
@@ -1,2 +0,0 @@
-const grapqlEndpoint : string = 'http://'+process.env.NEXT_PUBLIC_SERVER_HOST+':'+process.env.NEXT_PUBLIC_SERVER_PORT+'/graphql'
-export default grapqlEndpoint;
