191d2f05667dfb8c8ee16e7252882423a2445eee
KumailB <sxb180119@utdallas.edu>
2021-12-04 21:36:03 -0600

    prod: graphql refactor and remote host

--- client/pages/api/graphql/index.ts
+++ client/pages/api/graphql/index.ts
@@ -1,2 +1,2 @@
-const grapqlEndpoint : string = 'http://'+process.env.NEXT_PUBLIC_SERVER_HOST+':'+process.env.NEXT_PUBLIC_SERVER_PORT+'/graphql'
+const grapqlEndpoint : string = 'http://'+process.env.NEXT_PUBLIC_SERVER_HOST+( process.env.NEXT_PUBLIC_SERVER_PORT ? ':'+process.env.NEXT_PUBLIC_SERVER_PORT : '')+'/graphql'
 export default grapqlEndpoint;
